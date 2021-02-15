b9c3958ccc2aee7178a6aa73a1e43f00a401a65d
joel NOUMIA <joel.noumia@gfi.fr>
2021-02-15 23:36:52 +0100

    Premier test d'integration : user controller

---
+++ src/test/java/com/beezyweb/kewouGame/User/UserControllerTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.beezyweb.kewouGame.User;
+
+import com.beezyweb.kewouGame.controllers.UserController;
+import com.beezyweb.kewouGame.service.UserService;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.when;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+/**
+ *
+ * @author frup73532
+ */
+
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+    
+    @Autowired
+    private MockMvc mockMvc;
+    
+    @MockBean
+    private UserService service;
+    
+    
+    @Test
+    public void getUsers() throws Exception{              
+        this.mockMvc.perform(MockMvcRequestBuilders
+                .get("/users")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());             
+    }
+      
+}
